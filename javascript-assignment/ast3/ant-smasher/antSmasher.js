08e795cc790845a07fa22a736d6c62b28f9c2f5a
nischalshakya95 <nischalshakya95@gmail.com>
2019-10-10 09:51:29 +0545

    update ant smasher code

--- javascript-assignment/ast3/ant-smasher/antSmasher.js
+++ javascript-assignment/ast3/ant-smasher/antSmasher.js
@@ -1,172 +1,171 @@
-let MAX_WIDTH = 1200;
-let MAX_HEIGHT = 600;
+let MAX_WIDTH = 1000;
+let MAX_HEIGHT = 500;
 
-(function() {
-  function generateRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.ceil(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
+(function () {
+    function generateRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.ceil(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
 
-  function Ball(radius) {
-    this.element = null;
-    this.x = null;
-    this.y = null;
-    this.dx = 1;
-    this.dy = 1;
-    this.radius = null;
+    function Ant(radius) {
+        this.element = null;
+        this.x = null;
+        this.y = null;
+        this.dx = 1;
+        this.dy = 1;
+        this.radius = null;
 
-    this.init = () => {
-      this.radius = radius;
-      this.parentElement = document.getElementById("app");
-      this.element = document.createElement("div");
-      this.element.style.height = this.radius + "px";
-      this.element.style.width = this.radius + "px";
-      this.element.classList.add("box");
-      this.parentElement.appendChild(this.element);
-      return this;
-    };
-
-    this.init();
-
-    this.setPosition = (x, y) => {
-      this.x = x;
-      this.y = y;
-    };
-
-    this.draw = () => {
-      this.element.style.top = this.x + "px";
-      this.element.style.left = this.y + "px";
-      this.move();
-    };
-
-    this.move = () => {
-      this.x += this.dx;
-      this.y += this.dy;
-    };
-
-    this.isXWallCollision = () => {
-      return this.x + this.dx > MAX_HEIGHT - this.radius || this.x < 0;
-    };
-
-    this.reverseXDirection = () => {
-      this.dx *= -1;
-    };
-
-    this.isYWallCollision = () => {
-      return this.y + this.dy > MAX_WIDTH - this.radius || this.y < 0;
-    };
-
-    this.reverseYDirection = () => {
-      this.dy *= -1;
-    };
-
-    this.changeDirection = box => {
-      box.dx *= -1;
-      box.dy *= -1;
-      box.move();
-
-      this.dy *= -1;
-      this.dx *= -1;
-      this.move();
-    };
-  }
-
-  function Game(ballCount) {
-    this.colors = ["red", "green", "blue", "brown"];
-    this.ballCount = 0;
-    this.balls = [];
-    this.interval = null;
-    this.isBallMoving = true;
-    this.GAME_ANIMATION_FRAME = 10;
-    this.parentElement = null;
-    this.score = 0;
-
-    this.init = () => {
-      this.ballCount = ballCount;
-      this.createBall();
-      this.interval = setInterval(this.moveBalls.bind(this), 5);
-      this.parentElement = document.getElementById("app");
-      return this;
-    };
-
-    this.detectCollision = (ballOne, ballTwo) => {
-      let dx = ballOne.x + ballOne.radius - (ballTwo.x + ballTwo.radius);
-      let dy = ballOne.y + ballOne.radius - (ballTwo.y + ballTwo.radius);
-      let distance = Math.sqrt(dx * dx + dy * dy);
-
-      return distance < (ballOne.radius + ballTwo.radius) / 2;
-    };
-
-    this.detectAllCollision = () => {
-      for (let k = 0; k < this.balls.length; k++) {
-        for (let j = 0; j < this.balls.length; j++) {
-          if (k !== j) {
-            if (this.detectCollision(this.balls[k], this.balls[j])) {
-              this.balls[k].changeDirection(this.balls[j]);
-            }
-          }
-        }
-      }
-    };
-
-    this.moveBalls = () => {
-      for (let i = 0; i < this.balls.length; i++) {
-        if (this.balls[i].isXWallCollision()) {
-          this.balls[i].reverseXDirection();
-        }
-        if (this.balls[i].isYWallCollision()) {
-          this.balls[i].reverseYDirection();
-        }
-        this.balls[i].draw();
-        this.smash();
-      }
-      this.detectAllCollision();
-    };
-
-    this.playPause = () => {
-      if (this.isBallMoving) {
-        this.isBallMoving = false;
-        clearInterval(this.interval);
-      } else {
-        this.isBallMoving = true;
-        this.interval = setInterval(
-          this.moveBalls.bind(this),
-          this.GAME_ANIMATION_FRAME
-        );
-      }
-    };
-
-    this.createBall = () => {
-      for (let i = 0; i < this.ballCount; i++) {
-        let ball = new Ball(50);
-        let randomX = generateRandomInt(0, 500);
-        let randomY = generateRandomInt(0, 500);
-        ball.setPosition(randomX, randomY);
-        this.balls.push(ball);
-      }
-    };
-
-    this.smash = () => {
-      let ants = document.getElementsByClassName("box");
-      for (let i = 0; i < ants.length; i++) {
-        ants[i].onclick = () => {
-          this.balls.splice(i, 1);
-          this.parentElement.removeChild(ants[i]);
-          this.calculateScore();
-          this.displayScore();
+        this.init = () => {
+            this.radius = radius;
+            this.parentElement = document.getElementById('app');
+            this.element = document.createElement('div');
+            this.element.style.height = this.radius + 'px';
+            this.element.style.width = this.radius + 'px';
+            this.element.classList.add('ant');
+            this.parentElement.appendChild(this.element);
+            return this;
         };
-      }
-    };
 
-    this.calculateScore = () => {
-        this.score++;
-    };
+        this.init();
 
-    this.displayScore = () => {
-      document.getElementById('score').innerHTML = 'Score: ' + this.score;
-    };
-  }
+        this.setPosition = (x, y) => {
+            this.x = x;
+            this.y = y;
+        };
 
-  let game = new Game(20).init();
+        this.draw = () => {
+            this.element.style.top = this.x + 'px';
+            this.element.style.left = this.y + 'px';
+            this.move();
+        };
+
+        this.move = () => {
+            this.x += this.dx;
+            this.y += this.dy;
+        };
+
+        this.isXWallCollision = () => {
+            return this.x + this.dx > MAX_HEIGHT - this.radius || this.x < 0;
+        };
+
+        this.reverseXDirection = () => {
+            this.dx *= -1;
+        };
+
+        this.isYWallCollision = () => {
+            return this.y + this.dy > MAX_WIDTH - this.radius || this.y < 0;
+        };
+
+        this.reverseYDirection = () => {
+            this.dy *= -1;
+        };
+
+        this.changeDirection = ant => {
+            ant.dx *= -1;
+            ant.dy *= -1;
+            ant.move();
+
+            this.dy *= -1;
+            this.dx *= -1;
+            this.move();
+        };
+    }
+
+    function Game(antCount) {
+        this.antCount = 0;
+        this.ants = [];
+        this.interval = null;
+        this.isAntMoving = true;
+        this.GAME_ANIMATION_FRAME = 10;
+        this.parentElement = null;
+        this.score = 0;
+
+        this.init = () => {
+            this.antCount = antCount;
+            this.createAnt();
+            this.interval = setInterval(this.moveAnt.bind(this), 5);
+            this.parentElement = document.getElementById('app');
+            return this;
+        };
+
+        this.detectCollision = (antOne, antTwo) => {
+            let dx = antOne.x + antOne.radius - (antTwo.x + antTwo.radius);
+            let dy = antOne.y + antOne.radius - (antTwo.y + antTwo.radius);
+            let distance = Math.sqrt(dx * dx + dy * dy);
+
+            return distance < (antOne.radius + antTwo.radius) / 2;
+        };
+
+        this.detectAllCollision = () => {
+            for (let k = 0; k < this.ants.length; k++) {
+                for (let j = 0; j < this.ants.length; j++) {
+                    if (k !== j) {
+                        if (this.detectCollision(this.ants[k], this.ants[j])) {
+                            this.ants[k].changeDirection(this.ants[j]);
+                        }
+                    }
+                }
+            }
+        };
+
+        this.moveAnt = () => {
+            for (let i = 0; i < this.ants.length; i++) {
+                if (this.ants[i].isXWallCollision()) {
+                    this.ants[i].reverseXDirection();
+                }
+                if (this.ants[i].isYWallCollision()) {
+                    this.ants[i].reverseYDirection();
+                }
+                this.ants[i].draw();
+                this.smash();
+            }
+            this.detectAllCollision();
+        };
+
+        this.playPause = () => {
+            if (this.isAntMoving) {
+                this.isAntMoving = false;
+                clearInterval(this.interval);
+            } else {
+                this.isAntMoving = true;
+                this.interval = setInterval(
+                    this.moveAnt.bind(this),
+                    this.GAME_ANIMATION_FRAME
+                );
+            }
+        };
+
+        this.createAnt = () => {
+            for (let i = 0; i < this.antCount; i++) {
+                let ant = new Ant(50);
+                let randomX = generateRandomInt(0, 400);
+                let randomY = generateRandomInt(0, 400);
+                ant.setPosition(randomX, randomY);
+                this.ants.push(ant);
+            }
+        };
+
+        this.smash = () => {
+            let ants = document.getElementsByClassName('ant');
+            for (let i = 0; i < ants.length; i++) {
+                ants[i].onclick = () => {
+                    this.ants.splice(i, 1);
+                    this.parentElement.removeChild(ants[i]);
+                    this.calculateScore();
+                    this.displayScore();
+                };
+            }
+        };
+
+        this.calculateScore = () => {
+            this.score++;
+        };
+
+        this.displayScore = () => {
+            document.getElementById('score').innerHTML = 'Score: ' + this.score;
+        };
+    }
+
+    new Game(20).init();
 })();
