fb514e98add7b5fcdf3859fc36499266cf983bd1
nischalshakya95 <nischalshakya95@gmail.com>
2019-10-11 10:19:15 +0545

    Add movement controller to object

--- javascript-assignment/ast3/box-collision/bounceball.js
+++ javascript-assignment/ast3/box-collision/bounceball.js
@@ -1,160 +1,161 @@
-let MAX_WIDTH = 1200;
-let MAX_HEIGHT = 800;
+let MAX_WIDTH = 1000;
+let MAX_HEIGHT = 600;
 
 
-(function() {
-  function generateRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.ceil(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
+(function () {
+    function generateRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.ceil(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
 
-  function Ball(radius, color) {
-    this.element = null;
-    this.x = null;
-    this.y = null;
-    this.dx = 1;
-    this.dy = 1;
-    this.radius = null;
-    this.color = null;
+    function Ball(radius, color) {
+        this.element = null;
+        this.x = null;
+        this.y = null;
+        this.dx = 1;
+        this.dy = 1;
+        this.radius = null;
+        this.color = null;
 
-    this.init = () => {
-      this.radius = radius;
-      this.color = color;
-      this.parentElement = document.getElementById('app');
-      this.element = document.createElement('div');
-      this.element.style.height = this.radius + 'px';
-      this.element.style.width = this.radius + 'px';
-      this.element.style.background = this.color;
-      this.element.classList.add('box');
-      this.parentElement.appendChild(this.element);
-      return this;
-    };
+        this.init = () => {
+            this.radius = radius;
+            this.color = color;
+            this.parentElement = document.getElementById('app');
+            this.element = document.createElement('div');
+            this.element.style.height = this.radius + 'px';
+            this.element.style.width = this.radius + 'px';
+            this.element.style.background = this.color;
+            this.element.classList.add('box');
+            this.parentElement.appendChild(this.element);
+            return this;
+        };
 
-    this.init();
+        this.init();
 
-    this.setPosition = (x, y) => {
-      this.x = x;
-      this.y = y;
-    };
+        this.setPosition = (x, y) => {
+            this.x = x;
+            this.y = y;
+        };
 
-    this.draw = () => {
-      this.element.style.top = this.x + 'px';
-      this.element.style.left = this.y + 'px';
-      this.move();
-    };
+        this.draw = () => {
+            this.element.style.top = this.x + 'px';
+            this.element.style.left = this.y + 'px';
+            this.move();
+        };
 
-    this.move = () => {
-      this.x += this.dx;
-      this.y += this.dy;
-    };
+        this.move = () => {
+            this.x += this.dx;
+            this.y += this.dy;
+        };
 
-    this.isXWallCollision = () => {
-      return this.x + this.dx > MAX_HEIGHT - this.radius || this.x < 0;
-    };
+        this.isXWallCollision = () => {
+            return this.x + this.dx > MAX_HEIGHT - this.radius || this.x < 0;
+        };
 
-    this.reverseXDirection = () => {
-      this.dx *= -1;
-    };
+        this.reverseXDirection = () => {
+            this.dx *= -1;
+        };
 
-    this.isYWallCollision = () => {
-      return this.y + this.dy > MAX_WIDTH - this.radius || this.y < 0;
-    };
+        this.isYWallCollision = () => {
+            return this.y + this.dy > MAX_WIDTH - this.radius || this.y < 0;
+        };
 
-    this.reverseYDirection = () => {
-      this.dy *= -1;
-    };
+        this.reverseYDirection = () => {
+            this.dy *= -1;
+        };
 
-    this.changeDirection = box => {
-      box.dx *= -1;
-      box.dy *= -1;
-      box.move();
+        this.changeDirection = box => {
+            box.dx *= -1;
+            box.dy *= -1;
+            box.move();
 
-      this.dy *= -1;
-      this.dx *= -1;
-      this.move();
-    };
-  }
+            this.dy *= -1;
+            this.dx *= -1;
+            this.move();
+        };
+    }
 
-  function Game(ballCount) {
-    this.colors = ['red', 'green', 'blue', 'brown'];
-    this.ballCount = 0;
-    this.balls = [];
-    this.interval = null;
-    this.isBallMoving = true;
-    this.GAME_ANIMATION_FRAME = 10;
-
-    this.init = () => {
-      this.ballCount = ballCount;
-      this.createBall();
-      this.interval = setInterval(this.moveBalls.bind(this), 5);
-      return this;
-    };
-
-    this.detectCollision = (ballOne, ballTwo) => {
-      let dx = ballOne.x + ballOne.radius - (ballTwo.x + ballTwo.radius);
-      let dy = ballOne.y + ballOne.radius - (ballTwo.y + ballTwo.radius);
-      let distance = Math.sqrt(dx * dx + dy * dy);
-      return distance < (ballOne.radius + ballTwo.radius) / 2;
-    };
-
-    this.detectAllCollision = () => {
-      for (let k = 0; k < this.balls.length; k++) {
-        for (let j = 0; j < this.balls.length; j++) {
-          if (k !== j) {
-            if (this.detectCollision(this.balls[k], this.balls[j])) {
-              this.balls[k].changeDirection(this.balls[j]);
-            }
-          }
-        }
-      }
-    };
-
-    this.moveBalls = () => {
-      for (let i = 0; i < this.balls.length; i++) {
-        if (this.balls[i].isXWallCollision()) {
-          this.balls[i].reverseXDirection();
-        }
-        if (this.balls[i].isYWallCollision()) {
-          this.balls[i].reverseYDirection();
-        }
-        this.balls[i].draw();
-      }
-      this.detectAllCollision();
-    };
-
-    this.playPause = () => {
-      if (this.isBallMoving) {
-        this.isBallMoving = false;
-        clearInterval(this.interval);
-      } else {
+    function Game(ballCount) {
+        this.colors = ['red', 'green', 'blue', 'brown'];
+        this.radius = ['10', '20', '30', '40'];
+        this.ballCount = 0;
+        this.balls = [];
+        this.interval = null;
         this.isBallMoving = true;
-        this.interval = setInterval(
-          this.moveBalls.bind(this),
-          this.GAME_ANIMATION_FRAME
-        );
-      }
-    };
+        this.GAME_ANIMATION_FRAME = 10;
 
-    this.createBall = () => {
-      let index = 0;
-      for (let i = 0; i < this.ballCount; i++) {
-        if (index === this.colors.length) {
-          index = 0;
-        }
-        let ball = new Ball(50, this.colors[index]);
-        let randomX = generateRandomInt(0, 500);
-        let randomY = generateRandomInt(0, 500);
-        ball.setPosition(randomX, randomY);
-        this.balls.push(ball);
-        index += 1;
-      }
-    };
-  }
+        this.init = () => {
+            this.ballCount = ballCount;
+            this.createBall();
+            this.interval = setInterval(this.moveBalls.bind(this), 5);
+            return this;
+        };
 
-  let game = new Game(40).init();
+        this.detectCollision = (ballOne, ballTwo) => {
+            let dx = ballOne.x + ballOne.radius - (ballTwo.x + ballTwo.radius);
+            let dy = ballOne.y + ballOne.radius - (ballTwo.y + ballTwo.radius);
+            let distance = Math.sqrt(dx * dx + dy * dy);
+            return distance < (ballOne.radius + ballTwo.radius) / 2;
+        };
 
-  document.addEventListener('click', () => {
-    game.playPause();
-  });
+        this.detectAllCollision = () => {
+            for (let k = 0; k < this.balls.length; k++) {
+                for (let j = 0; j < this.balls.length; j++) {
+                    if (k !== j) {
+                        if (this.detectCollision(this.balls[k], this.balls[j])) {
+                            this.balls[k].changeDirection(this.balls[j]);
+                        }
+                    }
+                }
+            }
+        };
+
+        this.moveBalls = () => {
+            for (let i = 0; i < this.balls.length; i++) {
+                if (this.balls[i].isXWallCollision()) {
+                    this.balls[i].reverseXDirection();
+                }
+                if (this.balls[i].isYWallCollision()) {
+                    this.balls[i].reverseYDirection();
+                }
+                this.balls[i].draw();
+            }
+            this.detectAllCollision();
+        };
+
+        this.playPause = () => {
+            if (this.isBallMoving) {
+                this.isBallMoving = false;
+                clearInterval(this.interval);
+            } else {
+                this.isBallMoving = true;
+                this.interval = setInterval(
+                    this.moveBalls.bind(this),
+                    this.GAME_ANIMATION_FRAME
+                );
+            }
+        };
+
+        this.createBall = () => {
+            let index = 0;
+            for (let i = 0; i < this.ballCount; i++) {
+                if (index === this.colors.length) {
+                    index = 0;
+                }
+                let ball = new Ball(this.radius[index], this.colors[index]);
+                let randomX = generateRandomInt(0, 500);
+                let randomY = generateRandomInt(0, 500);
+                ball.setPosition(randomX, randomY);
+                this.balls.push(ball);
+                index += 1;
+            }
+        };
+    }
+
+    let game = new Game(40).init();
+
+    document.addEventListener('click', () => {
+        game.playPause();
+    });
 })();
--- javascript-assignment/ast4/flappybird/flappyBird.js
+++ javascript-assignment/ast4/flappybird/flappyBird.js
@@ -1,13 +1,97 @@
+let canvas = document.createElement("canvas");
+let context = canvas.getContext("2d");
+const ARROW_UP = "ArrowUp";
+const ARROW_DOWN = "ArrowDown";
+const ARROW_RIGHT = "ArrowRight";
+const ARROW_LEFT = "ArrowLeft";
+
 class GameArea {
+  constructor() {
+    return this;
+  }
 
-    constructor() {
-        this.canvas = document.createElement('canvas');
-    }
-
-    start() {
-        this.canvas.width = 480;
-        this.canvas.height = 500;
-    }
+  start() {
+    canvas.width = 480;
+    canvas.height = 500;
+    document.body.insertBefore(canvas, document.body.childNodes[0]);
+  }
 }
 
-new GameArea().start();
+class Bird {
+  isKeyPressed = true;
+  dx = 1;
+  dy = 1;
+  key = null;
+
+  constructor(x, y, width, height, color) {
+    this.setInitialPosition(x, y);
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.addEvent();
+    return this;
+  }
+
+  drawBird() {
+    context.fillStyle = this.color;
+    context.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  clear() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  setInitialPosition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  updateFrame() {
+    this.clear();
+    this.drawBird();
+    this.move();
+  }
+
+  move() {
+    if (this.isKeyPressed) {
+      switch (this.key) {
+        case ARROW_UP:
+          this.y += -this.dy;
+          break;
+        case ARROW_DOWN:
+          this.y += this.dy;
+          break;
+        case ARROW_RIGHT:
+          this.x += this.dx;
+          break;
+        case ARROW_LEFT:
+          this.x += -this.dx;
+          break;
+      }
+    }
+  }
+
+  addEvent() {
+    window.addEventListener("keydown", e => {
+      this.isKeyPressed = true;
+      this.key = e.key;
+    });
+
+    window.addEventListener("keyup", () => {
+      this.isKeyPressed = false;
+    });
+  }
+
+  interval() {
+    setInterval(this.updateFrame.bind(this), 10);
+  }
+}
+
+class Obstacle {
+    
+}
+
+let game = new GameArea();
+game.start();
+let bird = new Bird(10, canvas.height / 2, 20, 20, "red");
+bird.interval();
