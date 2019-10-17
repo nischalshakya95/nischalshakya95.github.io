50ffa362416a8d3fdad07550fd14bbb8d59fd001
nischalshakya95 <nischalshakya@lftechnology.com>
2019-10-17 14:34:26 +0545

    Add sprite animation in flappy bird of sprite-image folder

--- javascript-assignment/sprite-image/script.js
+++ javascript-assignment/sprite-image/script.js
@@ -1,29 +1,43 @@
-let canvas = document.getElementById('canvas');
-let context = canvas.getContext('2d');
+window.onload = function () {
+    let imageFramePoint = [0, 1, 2];
+    let imageFrameIndex = 0;
+    let frameCount = 0;
 
-// drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+    let dx = 40;
+    let dy = 40;
 
-class Sprite {
 
-    constructor() {
-        this.image = new Image();
-        this.image.src = './sprite.png';
-        this.sx = 0;
-        this.sy = 0;
-        this.sWidth = 24;
-        this.sHeight = 24;
-        this.dx = 10;
-        this.dy = 10;
-        this.dWidth = this.sWidth;
-        this.dHeight = this.sHeight;
-        return this;
-    }
+    let width = 35;
+    let height = 24;
 
-    drawImage() {
-        context.drawImage(this.image, this.sx, this.sy, this.sWidth, this.sHeight, this.dx, this.dy, this.dWidth, this.dWidth);
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext('2d');
+
+    let img = new Image();
+    img.src = 'sprite.png';
+    img.onload = function (e) {
+
+        function drawImage(frameX, frameY, canvasX, canvasY) {
+            context.drawImage(img, frameX * width, frameY * height, width, height, canvasX, canvasY, width, height);
+        }
+
+        function step() {
+            frameCount++;
+            if (frameCount < 10) {
+                requestAnimationFrame(step);
+                return;
+            }
+            frameCount = 0;
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            drawImage(imageFramePoint[imageFrameIndex], 0, dx, dy);
+            imageFrameIndex++;
+            if (imageFrameIndex >= imageFramePoint.length) {
+                imageFrameIndex = 0;
+            }
+            requestAnimationFrame(step);
+
+        }
+
+        requestAnimationFrame(step);
     }
 }
-
-let s = new Sprite();
-console.log(s);
-s.drawImage();
