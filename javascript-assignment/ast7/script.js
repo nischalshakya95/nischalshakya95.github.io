38d5142bc57a8f163c1fd483d7cf66fc4453cf10
nischalshakya95 <nischalshakya@lftechnology.com>
2019-10-18 09:59:07 +0545

    Add multiple balls

--- javascript-assignment/ast7/script.js
+++ javascript-assignment/ast7/script.js
@@ -3,15 +3,45 @@ let context = canvas.getContext('2d');
 
 class Helix {
 
+    balls = [];
+
+    constructor() {
+        this.initialX = 100;
+        this.initialY = 75;
+    }
+
+    loop(){
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        this.create();
+        this.draw();
+        requestAnimationFrame(this.loop.bind(this));
+    }
+
+    create() {
+        for (let i = 0; i < 10; i++) {
+            let ball = new Ball(this.initialX, this.initialY);
+            this.initialY += 30;
+            this.balls.push(ball);
+        }
+    }
+
+    draw() {
+        for (let i = 0; i < this.balls.length; i++) {
+            this.balls[i].update();
+            this.balls[i].draw();
+        }
+    }
+
 }
 
 class Ball {
-    constructor() {
+
+    constructor(x, y) {
         this.amplitude = 50;
         this.frequency = 0.01;
         this.increament = this.frequency;
-        this.x = 100;
-        this.y = 75;
+        this.x = x;
+        this.y = y;
         this.radius = 5;
         this.dummyX = 0;
         this.offset = this.y;
@@ -19,22 +49,15 @@ class Ball {
         this.speed = 1;
     }
 
-    loop() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        this.update();
-        this.draw();
-        requestAnimationFrame(this.loop.bind(this));
-    }
-
     draw() {
         context.beginPath();
         context.fillStyle = 'blue';
         context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
         context.closePath();
-        context.fill();   
+        context.fill();
     }
 
-    update(){
+    update() {
         this.dummyX++;
         this.y = this.amplitude * Math.sin(this.speed * this.dummyX * Math.PI / 180) + this.offset;
         this.radius = this.radiusAmplitude * Math.cos(this.speed * this.dummyX * Math.PI / 180) + this.radiusAmplitude + 1;
@@ -42,5 +65,5 @@ class Ball {
 
 }
 
-let h = new Ball();
+let h = new Helix();
 h.loop();
