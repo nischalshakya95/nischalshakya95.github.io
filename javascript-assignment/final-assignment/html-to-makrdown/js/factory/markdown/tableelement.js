3b5fc79a04c936cc75a6fd94ec8942762cb6ea05
nischalshakya95 <nischalshakya@lftechnology.com>
2019-10-24 14:29:38 +0545

    generate table heading

--- javascript-assignment/final-assignment/html-to-makrdown/js/factory/markdown/tableelement.js
+++ javascript-assignment/final-assignment/html-to-makrdown/js/factory/markdown/tableelement.js
@@ -1,14 +1,28 @@
 class TableElement extends ElementFactory {
 
+    constructor() {
+        super();
+        this.heading = '';
+    }
+
     replace(node, tag) {
         console.log(node.rows);
         if (node.rows.length !== 0) {
             for (let i = 0; i < node.rows.length; i++) {
                 for (let j = 0; j < node.rows[i].cells.length; j++) {
-                    console.log(node.rows[i].cells[j]);
+                    let row = node.rows[i].cells[j];
+                    if (row.nodeName.toLowerCase() === 'th') {
+                        this.heading += this.replaceHeading(row.innerText.trim());
+                    }
+
                 }
             }
+            return this.heading + ' |';
         }
+    }
+
+    replaceHeading(content) {
+        return '| ' + content + ' ';
     }
 
 }
