e670f0c7bd9731c0d3649c2fd76a2c42c073fcf9
nischalshakya95 <nischalshakya95@lftechnology.com>
2019-10-30 19:36:39 +0545

    Fix multiple whitespace in between

--- javascript-assignment/final-assignment/html-to-markdown/js/factory/markdown/paragraph-element.js
+++ javascript-assignment/final-assignment/html-to-markdown/js/factory/markdown/paragraph-element.js
@@ -1,9 +1,23 @@
 class ParagraphElement extends ElementFactory {
 
+    emptyMsg = 'Empty paragraph';
+
     replace(node, tag, str) {
-        if (node.childElementCount > 0) {
-            this.getChildElement().getChildNodes(node);
+        if (str !== null) {
+            if (this.validate(str, tag)) {
+                if (node.childElementCount > 0) {
+                    this.getChildElement().getChildNodes(node);
+                }
+                return node.textContent.replace(/\s+/g, ' ') + '\n\n';
+            } else {
+                return "Invalid ending tag";
+            }
+        } else {
+            return this.emptyMsg;
         }
-        return node.textContent + '\n\n';
+    }
+
+    validate(str, tag) {
+        return str.endsWith('</' + tag.toLowerCase() + '>');
     }
 }
--- javascript-assignment/final-assignment/html-to-markdown/js/test.js
+++ javascript-assignment/final-assignment/html-to-markdown/js/test.js
@@ -1,4 +1,7 @@
-let str = '<h1>nischal shakya<h2></h1>';
-let regexOne = /(<h1>\w(<|>)|<\/h1>)/ig;
-let regex = /\s*(?!^<h1>)(?!<\/h1>$)(<h[1-6]>.*|<\/h[1-6]>.*)/ig;
-console.log(regexOne.test(str));
+// let str = '<h1>nischal shakya<h2></h1>';
+// let regexOne = /(<h1>\w(<|>)|<\/h1>)/ig;
+// let regex = /\s*(?!^<h1>)(?!<\/h1>$)(<h[1-6]>.*|<\/h[1-6]>.*)/ig;
+// console.log(regexOne.test(str));
+
+let str = 'hello world   nischal shakya ';
+console.log(str.replace(/\s+/g, ' '));
